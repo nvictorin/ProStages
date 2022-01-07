76733b0fde96ff2739f95585db97e9ee9b8756bb
Nathan Victorin <nvictorin@iutbayonne.univ-pau.fr>
2022-01-07 20:42:23 +0100

    Mise en place de nouvelles routes

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -35,6 +35,16 @@ class ProStagesController extends AbstractController
         return $this->render('pro_stages/entreprises.html.twig',['entreprises'=>$entreprises]);
     }
 
+    public function entreprisesStages($id): Response
+    {
+        //Récupérer le repository de l'entité Entreprise
+        $repositoryStagesEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+        //Récupérer les ressources enregistrées en BD
+        $stagesEntreprises = $repositoryStagesEntreprise->findAll();
+        //Envoyer les ressources récupérées à la vue chargée de les afficher
+        return $this->render('pro_stages/entreprises.html.twig',['stagesEntreprises'=>$stagesEntreprises]);
+    }
+
     public function formations(): Response
     {
         //Récupérer le repository de l'entité Formation
@@ -43,6 +53,16 @@ class ProStagesController extends AbstractController
         $formations = $repositoryFormation->findAll();
         //Envoyer les ressources récupérées à la vue chargée de les afficher
         return $this->render('pro_stages/formations.html.twig',['formations'=>$formations]);
+    }
+
+    public function formationsStages($id): Response
+    {
+        //Récupérer le repository de l'entité Formation
+        $repositoryStagesFormation = $this->getDoctrine()->getRepository(Formation::class);
+        //Récupérer les ressources enregistrées en BD
+        $stagesFormations = $repositoryStagesFormation->findAll();
+        //Envoyer les ressources récupérées à la vue chargée de les afficher
+        return $this->render('pro_stages/formations.html.twig',['stagesFormations'=>$stagesFormations]);
     }
 
     public function stages($id): Response
